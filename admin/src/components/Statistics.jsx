8e584bb101fe7bf76d33f606d971811420db2181
Jérémy Tahar <jeremytahar@mac.home>
2025-03-19 17:29:59 +0100

    Added react backoffice files

---
+++ admin/src/components/Statistics.jsx
@@ -0,0 +1,13 @@
+// stats page
+
+import { Link } from 'react-router-dom'
+
+const Statistics = () => {
+    return (
+        <div>
+        <h1>Statistiques</h1>
+        </div>
+    )
+}
+
+export default Statistics
