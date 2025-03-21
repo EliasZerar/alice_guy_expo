4934f2e7d524e26efb1d7999583112760b11e1d4
miladup <miladuperrier@gmail.com>
2025-03-21 22:32:24 +0100

    adding alice section and modifications on loading page

---
+++ js/loading.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    const loadingPage = document.getElementById('loading-page');
+
+    const hasVisited = sessionStorage.getItem('hasVisited');
+
+    if (!hasVisited) {
+        sessionStorage.setItem('hasVisited', 'true');
+
+        setTimeout(() => {
+            loadingPage.style.opacity = 0;
+            setTimeout(() => {
+                loadingPage.remove();
+            }, 500); // Durée de la transition CSS
+        }, 3100); // Attente avant de faire disparaître la landing page
+    } else {
+        loadingPage.remove();
+    }
+};
